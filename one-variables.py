9382b5ad93a2a7772e8fb950931708fbdf5fb82a
Edith <mxedithbaltazar@gmail.com>
2023-06-19 10:28:19 -0700

    first commit

---
+++ one-variables.py
@@ -0,0 +1,56 @@
+# name = "Taco Time"
+
+# print(name) # make sure it is not with quotes
+# print("Hello " + name) # we can combine a string (str) with another string 
+# # print(type(name)) # to check datatype of variable -- <class 'str'>
+
+# first_name = "First"
+# last_name = "Last"
+
+# # full_name = first_name + last_name
+# # print("Hello " + full_name) # Hello FirstLast
+
+# full_name = first_name + " " + last_name
+# print("Hello " + full_name) # Hello First Last
+
+# age = "21" # this would still be a string
+# age = 21 # this is an int
+# age = age + 1 
+# print(age) # 22
+
+# age += 1 # shortcut of age + integer
+# print(age) # 22
+# print(type(age)) # <class 'int'>
+
+# age = "21"
+# print(type(age)) # <class 'str'>
+
+# age  = 21
+# age += 1
+# print("Your age is: " + age) # TypeError: can only concatenate str (not "int") to str
+
+# this is typecasting -- age is now a stringn using string casting
+# print("Your age is: " + str(age)) # Your age is 22
+
+# float = floating point number (a decimal number)
+    # integer (int) datatype cannot store a decimal portion - an int only stores a whole number
+# height = 250.5 # floating point number - float for short
+# print(height) # 250.5
+# print(type(height)) # <class 'float'>
+# # now create a str literal and use type casting to convert height into a str
+# print("Your height is: " + str(height)) # Your height is: 250.5
+# print("Your height is: " + str(height) + "cm") # Your height is: 250.5cm
+
+# boolean -- True or False
+    # variable that only stores True or False
+human = True # or False
+print(human) # True
+print(type(human)) # <class 'bool'>
+# don't put boolean in quotes data type will change to str -- see that the color between str and bool changes
+# human = "True"
+# now what if you need to display your value along with str using str concatenation -- typecast
+print("Are you a human: " + str(human)) # Are you a human: True
+
+
+
+
---
+++ python.md
@@ -0,0 +1,34 @@
+# Python Tutorial
+
+    - install python
+    - install integrated developement enviornment ie PyCharm, VSCode
+      - this is a software that helps us write other software
+
+## Create New Project
+
+    - Go onto IDE and create new project
+      - Name project "Hello World"
+    - Terminal window
+    - print a message
+      - print('Hello World') -- run -- see terminal window of printed string
+      - print("Hello World") -- run -- see terminal window of printed string
+
+## Variables - Pt 1
+    - a variable = container for a value. behaves as the value that it contains
+    - 4 basic datatypes
+      - strings (str): which store a series of characters 
+      - integers (int): which store a whole number 
+      - floating point number (float): a numeric vlaue with a decimal
+      - booleans (bool): which only store True or False, very useful with if statements
+        - ie x = 2 + 5 x = 7
+    - variable string "Hello"
+    - variable integer 21
+    - variable float 2.5 
+    - variable boolean false/true
+  - You can combine variables together as long as they are of the same datatype
+
+- the reason you may want to use booleans instead of str even though you can still store a str representation of the word false or true is that, these are very useful when we get to if statements. we can check to see if some statement is true/false
+
+## String methods
+
+  - 
